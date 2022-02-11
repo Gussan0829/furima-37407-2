83b9af515284dc6e960903c674dc079b87bfb816
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-11 11:51:44 +0900

    purchasesテーブルとshippingテーブルの作成

--- db/migrate/20220211023841_create_shippings.rb
+++ db/migrate/20220211023841_create_shippings.rb
@@ -1,7 +1,8 @@
 class CreateShippings < ActiveRecord::Migration[6.0]
   def change
     create_table :shippings do |t|
-
+      t.references :user ,null: false, foreign_key: true
+      t.references :item ,null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20220211023944_create_purchases.rb
+++ db/migrate/20220211023944_create_purchases.rb
@@ -1,7 +1,12 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-
+      t.string     :post_code    ,null: false
+      t.string     :municipality ,null: false
+      t.string     :address      ,null: false
+      t.string     :building_name
+      t.integer    :phone_number ,null: false
+      t.references :purchase     ,null: false, foregin_key: true
       t.timestamps
     end
   end
