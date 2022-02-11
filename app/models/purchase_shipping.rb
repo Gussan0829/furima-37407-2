5435d480e3ac130bedd1b091a5c15fc2206a69df
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-11 21:11:26 +0900

    モデルの設定
    purchase_shippingモデルの編集
    バリデーションの追加（電話番号・郵便番号）

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,20 +1,27 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :user,:item, :post_code,:prefecture_id, :municipality, :address, :building_name, :phone_number, :purchase 
+  attr_accessor :user_id, :item_id, :post_code,:prefecture_id, :municipality, :address, :building_name, :phone_number, :purchase_id 
 
   with_options presence: true do
     validates :post_code  
-    validates :municipality
-    validates :address      
+    validates :municipality     
     validates :building_name
     validates :prefecture_id
-    validates :purchase, foreign_key: true
-    validates :user, foreign_key: true
-    validates :item, foreign_key: true
+    validates :purchase
+    validates :user    
+    validates :item    
+  end
+
+  with_options presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ } do
+    validates :address
+  end
+
+  with_options presence: true, format: { with: /^\d{10}$|^\d{11}$/ } do
+    validates :phone_number
   end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Shipping.create(post_code: post_code, municipality: municipality, address: address, building_name: building_name, prefecture_id: prefecture_id, purchase: purchase.id)
+    Shipping.create(post_code: post_code, municipality: municipality, address: address, building_name: building_name, prefecture_id: prefecture_id, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
