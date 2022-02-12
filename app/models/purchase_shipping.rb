164b90a38676ac1f9567c6b6ea44ab1769b190a8
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-12 19:24:59 +0900

    モデルの修正

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,22 +1,22 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code,:prefecture_id, :municipality, :address, :building_name, :phone_number, :purchase_id 
+  attr_accessor :user_id, :item_id, :post_code,:prefecture_id, :municipality, :address, :building_name, :phone_number, :purchase_id
 
   with_options presence: true do
     validates :post_code  
     validates :municipality     
     validates :building_name
     validates :prefecture_id
-    validates :purchase
-    validates :user    
-    validates :item    
+    validates :purchase_id
+    validates :user_id    
+    validates :item_id    
   end
 
   with_options presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ } do
     validates :address
   end
 
-  with_options presence: true, format: { with: /^\d{10}$|^\d{11}$/ } do
+  with_options presence: true, format: { with: /\A\d{10,11}\z/ } do
     validates :phone_number
   end
 
