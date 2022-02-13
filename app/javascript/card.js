cbec3821114fe97c93a9ed483de768f69233c236
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-14 00:15:43 +0900

    カード情報の追加
    tokenの取得
    サーバーサイドへのデータの送信
    データ送信時、カード情報の削除

---
+++ app/javascript/card.js
@@ -0,0 +1,34 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_5c368666ccd6b702fd035187");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_shipping[number]"),
+      cvc: formData.get("purchase_shipping[cvc]"),
+      exp_month: formData.get("purchase_shipping[exp_month]"),
+      exp_year: `20${formData.get("purchase_shipping[exp_year]")}`,
+    };
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("card_number").removeAttribute("name");
+      document.getElementById("card_cvc").removeAttribute("name");
+      document.getElementById("card_exp_month").removeAttribute("name");
+      document.getElementById("card_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
+  });
+};
+
+window.addEventListener("load", pay);
