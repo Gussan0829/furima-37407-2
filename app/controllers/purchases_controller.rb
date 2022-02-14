730390b467c05b38971f289495315925f78013c3
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-14 15:49:30 +0900

    リファクタリング
    ＠itemのリファクタリング

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,9 @@
 class PurchasesController < ApplicationController
+  before_action :item_find, only:[:index,:create]
   before_action :move_to_index, only:[:index]
 
   def index
     @purchase_shipping = PurchaseShipping.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -38,6 +38,10 @@ class PurchasesController < ApplicationController
         card: purchase_params[:token],   
         currency: 'jpy'                 
       )
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
   end
 
 end
