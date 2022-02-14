1b0e6c4bb872246e5861bde9ca6c9b1fe227c45c
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-14 15:45:48 +0900

    環境変数の設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,6 +10,7 @@ class PurchasesController < ApplicationController
     @purchase_shipping = PurchaseShipping.new(purchase_params)
     @item = Item.find(params[:item_id])
     if @purchase_shipping.valid?
+      pay_item
       @purchase_shipping.save
       return redirect_to root_path
     else
@@ -20,7 +21,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_shipping).permit(:post_code, :municipality, :address, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_shipping).permit(:post_code, :municipality, :address, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def move_to_index
@@ -28,6 +29,15 @@ class PurchasesController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price, 
+        card: purchase_params[:token],   
+        currency: 'jpy'                 
+      )
   end
 
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_5c368666ccd6b702fd035187");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
@@ -13,6 +13,7 @@ const pay = () => {
       exp_month: formData.get("purchase_shipping[exp_month]"),
       exp_year: `20${formData.get("purchase_shipping[exp_year]")}`,
     };
+    
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
@@ -21,10 +22,11 @@ const pay = () => {
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
 
-      document.getElementById("card_number").removeAttribute("name");
-      document.getElementById("card_cvc").removeAttribute("name");
-      document.getElementById("card_exp_month").removeAttribute("name");
-      document.getElementById("card_exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      
 
       document.getElementById("charge-form").submit();
     });
